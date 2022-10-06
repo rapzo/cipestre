beacb51a11615a3370393c306e406be6985fb0b0
Rui Lima <ruipedro.lima@gmail.com>
2022-10-06 17:38:00 +0100

    Add mochawesome-reports to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'cypress';
+import * as cypressMochawesomeReporter from 'cypress-mochawesome-reporter/lib';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+    code: false,
+    saveHtml: true,
+  },
   e2e: {
     specPattern: `${__dirname}/cypress/e2e/**/*.spec.ts`,
     baseUrl: 'https://cipestre.web.app/',
     video: false,
+    setupNodeEvents(on) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await cypressMochawesomeReporter.beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await cypressMochawesomeReporter.afterRunHook();
+      });
+    },
   },
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,5 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+import 'cypress-mochawesome-reporter/register';
+
 // Import commands.js using ES2015 syntax:
 import './commands';
